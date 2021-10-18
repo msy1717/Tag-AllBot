6a47e7ec411d325a31af8a0d228c17c0a3e5a2f5
Mrunal <80758264+msy1717@users.noreply.github.com>
2021-10-18 21:30:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import os, logging, asyncio
 
-from telegraph import upload_file
+
 from telethon import Button
 from telethon import TelegramClient, events
 from telethon.sessions import StringSession
