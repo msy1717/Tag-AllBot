a81ce9f4b4263a22e3333db0791ecc4cdc7c4c24
Mrunal <80758264+msy1717@users.noreply.github.com>
2021-10-19 15:48:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,6 @@ from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 from telethon.tl.types import ChannelParticipantsAdmins
 from telethon.events import NewMessage
-telegraph = Telegraph()
 
 
 
@@ -123,83 +122,6 @@ async def mentionall(event):
 
 
 #yaha kuch kachra tha ya fr extra features 🥲🥲removed now
-@client.on(events.NewMessage(pattern="^/t$"))
-
-async def telegraph(client, message):
-
-    replied = message.reply_to_message
-
-    if not replied:
-
-        await message.reply("Reply to a supported media file")
-
-        return
-
-    if not (
-
-        (replied.photo and replied.photo.file_size <= 5242880)
-
-        or (replied.animation and replied.animation.file_size <= 5242880)
-
-        or (
-
-            replied.video
-
-            and replied.video.file_name.endswith(".mp4")
-
-            and replied.video.file_size <= 5242880
-
-        )
-
-        or (
-
-            replied.document
-
-            and replied.document.file_name.endswith(
-
-                (".jpg", ".jpeg", ".png", ".gif", ".mp4"),
-
-            )
-
-            and replied.document.file_size <= 5242880
-
-        )
-
-    ):
-
-        await message.reply("Not supported!")
-
-        return
-
-    download_location = await client.download_media(
-
-        message=message.reply_to_message,
-
-        file_name="root/downloads/",
-
-    )
-
-    try:
-
-        response = upload_file(download_location)
-
-    except Exception as document:
-
-        await message.reply(message, text=document)
-
-    else:
-
-        await message.reply(
-
-            f"**Hey You...!\nLoook At This\n\n👉 https://telegra.ph{response[0]}**",
-
-            disable_web_page_preview=True,
-
-        )
-
-    finally:
-
-        os.remove(download_location)
 
 print("Started Successfully Join Support")
 print("¯\_(ツ)_/¯ Need Help Join @Botz_Official")
